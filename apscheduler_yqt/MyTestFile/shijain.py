2399e0f2adc6f9616f9cf4f35d1f6e0af2b872fe
hikekang <38242754+hikekang@users.noreply.github.com>
2021-04-01 22:15:08 +0800

    获取文件夹下面某一类文件以及post请求上传xlsx文件

--- apscheduler_yqt/MyTestFile/shijain.py
+++ apscheduler_yqt/MyTestFile/shijain.py
@@ -6,14 +6,17 @@
    time：          2021/3/30 15:48
 """
 import datetime
+# 格式化时间
 print(datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d %H:%M:%S')  )
 print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') )
 print(datetime.datetime.now().strftime('%Y-%m-%d %H')+":00:00" )
+# 往前算一小时
 one_hour_ago=datetime.datetime.now()-datetime.timedelta(hours=1)
 print(one_hour_ago)
 end_time=one_hour_ago.strftime('%Y-%m-%d %H')+":00:00"
 print(end_time)
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+# 根据指定的格式把一个时间字符串解析为时间元组。
 end_time1=datetime.datetime.strptime(end_time,"%Y-%m-%d %H:%M:%S")
 print(type(end_time1))
 print(type(end_time))
