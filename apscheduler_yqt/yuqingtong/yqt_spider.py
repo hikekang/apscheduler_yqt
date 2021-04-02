fe3dc73665f4a48c08981a86b085d53193ecb827
hikekang <38242754+hikekang@users.noreply.github.com>
2021-04-02 11:23:59 +0800

    测试自动抓取并且记录

--- apscheduler_yqt/yuqingtong/yqt_spider.py
+++ apscheduler_yqt/yuqingtong/yqt_spider.py
@@ -599,7 +599,7 @@ class YQTSpider(object):
                 record_file_path=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
                              f"record\{config.info['project_name']}",f"{self}_'记录'.xlsx")
 
-                sql_number=ssql_helper.find_info_count(tim['start_time'],tim['end_time'])
+                sql_number=ssql_helper.find_info_count(self.interval[0],self.interval[1])
                 data_list=[config.info['project_name'],datetime.datetime.now().strftime('%Y-%m-%d'),self.last_end_time,self.next_end_time]
                 SpiderHelper.save_record(record_file_path,yqt_count,xlsx_num,post_info['number'],sql_number,data_list=data_list)
                 return True
@@ -663,9 +663,10 @@ def work_it():
     yqt_spider = YQTSpider(start_time=start_time, end_time=end_time)
     yqt_spider.start(start_time=start_time, end_time=end_time, time_sleep=2)
 def apscheduler():
-    trigger1 = CronTrigger(hour='9-18', minute='*/59', second=0, jitter=30)
+    trigger1 = CronTrigger(hour='9-18', minute='*/1', second=0, jitter=30)
     sched = BlockingScheduler()
     sched.add_job(work_it, trigger1, id='my_job_id')
     sched.start()
+
 if __name__ == '__main__':
     apscheduler()
