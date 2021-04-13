e19863766a953c1b239925bf989f0834135b220d
hikekang <38242754+hikekang@users.noreply.github.com>
2021-04-13 10:45:48 +0800

    添加去掉表情符号

--- apscheduler_yqt/utils/baidu_emition.py
+++ apscheduler_yqt/utils/baidu_emition.py
@@ -6,13 +6,21 @@
    time：          2021/4/10 10:55
 """
 from aip import AipNlp
-
-
+import re
 APP_ID="18295264"
 API_KEY="ooX8tfe05iQ8Cb1tjxY5xGx7"
 SECRET_KEY="6EOG69bnEu6HsCQv0yOBO4okdrGwh3qm"
 
 def emotion(text):
     client=AipNlp(APP_ID,API_KEY,SECRET_KEY)
-    data=client.sentimentClassify(text)
+    data=client.sentimentClassify(re_emojis(text))
     return data['items'][0]['positive_prob']
+
+def re_emojis(text):
+    emoji_pattern = re.compile("["
+           u"\U0001F600-\U0001F64F"
+           u"\U0001F300-\U0001F5FF"
+           u"\U0001F680-\U0001F6FF"
+           u"\U0001F1E0-\U0001F1FF"
+           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r' ', text)
