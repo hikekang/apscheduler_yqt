140f5b1ae728859708cff2a919bd75ff11d4d174
hikekang <38242754+hikekang@users.noreply.github.com>
2021-04-21 10:20:42 +0800

    添加了限制处理

--- apscheduler_yqt/MyTestFile/baidu_emotion_sdk.py
+++ apscheduler_yqt/MyTestFile/baidu_emotion_sdk.py
@@ -13,7 +13,7 @@ SECRET_KEY="6EOG69bnEu6HsCQv0yOBO4okdrGwh3qm"
 from aip import AipNlp
 client=AipNlp(APP_ID,API_KEY,SECRET_KEY)
 
-text = "招聘驾驶员、装卸工、合伙人戳这里关注我，看最新招聘信息👇公司信息:单位:海门市壹米滴答物流地址:海门市三和镇滨港大道4881号壹米滴答物流岗位一：B、C照驾驶员岗位要求："
+text = "15309347317 ➤旺铺转让 【A001】我公司负责镇原县安能物流（全境），各项手续齐全。现月均到货量30吨--50吨，有稳定的客户资源，企业客户利润空间大，盈利可观。接手即可运转盈利。因其他原因，一个人经营管理太累，现对外一次性转让。"
 time1=time.time()
 # data=client.sentimentClassify(text)
 # print(data)
@@ -21,7 +21,7 @@ time1=time.time()
 # time2=time.time()
 # print(time2-time1)
 
-
+print(client.sentimentClassify(text))
 def re_emojis(text):
     emoji_pattern = re.compile("["
            u"\U0001F600-\U0001F64F"
