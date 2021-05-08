f2731542bdb512948b1c5c0bc4282a2776723f42
hikekang <38242754+hikekang@users.noreply.github.com>
2021-05-08 14:35:53 +0800

    增加日志文件自动创建

--- apscheduler_yqt/utils/mylogger.py
+++ apscheduler_yqt/utils/mylogger.py
@@ -5,14 +5,25 @@
 # @File    : mylogger.py
 import logging
 from logging.handlers import RotatingFileHandler
+import sys
 import os
-
 logger = logging.getLogger()
 # print(type(logger))
 logger.setLevel(logging.DEBUG)
 # 创建一个handler，用于写入日志文件
 # print(__name__)
 # fh = logging.FileHandler('./log/test2.log', encoding="utf-8")
+print(os.getcwd())
+dir_path = os.path.dirname(os.getcwd())
+dir_path=os.path.join(dir_path,'log')
+log_path = os.path.join(dir_path, 'test.log')
+if not os.path.exists(dir_path):
+    os.mkdir(dir_path)
+    print("创建成功")
+print(dir_path)
+if not os.path.exists(log_path):
+    with open(log_path,"wb") as f:
+        f.close()
 fh = RotatingFileHandler('../log/test.log', mode='a', maxBytes=5 * 1024 * 1024, backupCount=2, delay=0, encoding="utf-8")
 
 fh.setLevel(logging.INFO)
@@ -36,8 +47,9 @@ class MyLogger(object):
 
 
 if __name__ == '__main__':
-    logger.debug('logger debug message')
-    logger.info('logger info message')
-    logger.warning('logger warning message')
-    logger.error('logger error message')
-    logger.critical('logger critical message')
+    # logger.debug('logger debug message')
+    # logger.info('logger info message')
+    # logger.warning('logger warning message')
+    # logger.error('logger error message')
+    # logger.critical('logger critical message')
+    print(1)
